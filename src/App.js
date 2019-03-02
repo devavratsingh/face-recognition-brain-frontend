4808aff7f90faeaa6ddef9bef55ac44840673915
DEVAVRAT SINGH <43301449+satsaiinfocom@users.noreply.github.com>
2019-03-03 02:21:30 +0530

    frontend codereview ended, security improoved

--- src/App.js
+++ src/App.js
@@ -7,12 +7,7 @@ import Signin from './components/Signin/Signin';
 import Register from './components/Register/Register';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 import './App.css';
-
-const app = new Clarifai.App({
- apiKey: '0588606d0a2f4355a73db0842e2d247f'
-});
 
 const particlesOptions = {
   particles: {
@@ -26,13 +21,27 @@ const particlesOptions = {
 }
 }
 
+const initialState = {
+  input: '',
+  imageUrl:'',
+  box: {},
+  route:'signin',
+  isSignedIn:false,
+  user: {
+    id:'',
+    name: "",
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+}
 class App extends Component {
   constructor() {
     super();
     this.state = {
       input: '',
       imageUrl:'',
-      box: '',
+      box: {},
       route:'signin',
       isSignedIn:false,
       user: {
@@ -58,7 +67,7 @@ class App extends Component {
   onRouteChange = (route) => {
     this.setState({route: route});
     if(route === 'signout'){
-      this.setState({isSignedIn:false})
+      this.setState(initialState)
     } else if(route === 'home') {
       this.setState({isSignedIn:true})
     }
@@ -87,10 +96,14 @@ class App extends Component {
   }
   onButtonSubmit = () => {
     this.setState({imageUrl:this.state.input})
-    app.models
-    .predict(
-      Clarifai.FACE_DETECT_MODEL, 
-      this.state.input)
+    fetch('http://localhost:3000/imageurl', {
+          method:'post',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            input: this.state.input
+          })
+        })
+    .then(response => response.json())
     .then(response => {
       if(response) {
         fetch('http://localhost:3000/image', {
@@ -104,6 +117,7 @@ class App extends Component {
         .then(count => {
           this.setState(Object.assign(this.state.user, {entries: count}))
         })
+        .catch(console.log)
       }
       this.displayFaceBox(this.calculateFaceLocation(response))
     })
